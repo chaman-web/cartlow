9e598dd634964242fa2fa7b0a0c2c98c155410b8
Your Name <chamanjutt786@gmail.com>
2024-12-20 17:13:43 +0500

    remove some extra things

--- test/login/test_login.py
+++ test/login/test_login.py
@@ -15,17 +15,9 @@ def test_login_google(set_up_tear_down) -> None:
 def test_get_category(set_up_tear_down) -> None:
     page = set_up_tear_down
     # Select all 'li > a' elements
-    locator = page.locator("li > a").all_text_contents()
-    count = locator.count()
+    all_texts = page.locator("li > a").all_text_contents()
 
-    print(f"Total number of links: {count}")
-
-    # If you want to print the text of each link
-    for i in range(count):
-        print(f"Link Text {i + 1}: {locator.nth(i).text_content()}")
-
-    # all_texts.count()
-    # # Print the text of each link
-    # for text in all_texts:
-    #     print(f"Link Text: {text}")  # Indent this line properly
+    # Print the text of each link
+    for text in all_texts:
+        print(f"Link Text: {text}")  # Indent this line properly
 
